2b2d76a180bdb0a6bd3b4f0f461e4c99e34b0099
scongebop <littlefast2@gmail.com>
2023-02-13 12:36:19 +0800

    Updated test

--- tests/get_table.cpp
+++ tests/get_table.cpp
@@ -14,18 +14,19 @@ extern "C"
 int main()
 {
 	printf("Test: Getting values from a table\n");
-	std::unique_ptr<lua> L(new lua());
+	Lua L(new lua());
 	L -> openlib_all();
-	std::unique_ptr<table> globals = L -> global_table();
+	LuaTable globals = L -> global_table();
 
 	globals -> set_string("my_str", "Hello World");
 	const char* x = globals -> get_string("my_str");
 	
 	printf("the variable `my_str` has a string value of: \"%s\"\n", x);
-	if (strcmp(x, "Hello World")==0)
+	if (strcmp(x, "Hello World")!=0)
 	{
-		return 0;
-	}else{
+		printf("Test failed\n");
 		return 1;
 	}
+
+
 }
